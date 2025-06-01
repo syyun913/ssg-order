4e87f134959e2c85f306450aabaef77d8a95e2f8
syyun <sy.yun913@gmail.com>
2025-06-01 23:25:47 +0900

    design: 인증/인가 구현
    로그인, 회원가입 화면 추가
    
    Related to: #4

---
+++ src/main/java/com/ssg/order/api/user/controller/UserViewController.java
@@ -0,0 +1,20 @@
+package com.ssg.order.api.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserViewController {
+
+    @GetMapping("/login-page")
+    public String loginPage() {
+        return "user/login";
+    }
+
+    @GetMapping("/register-page")
+    public String registerPage() {
+        return "user/register";
+    }
+} 
