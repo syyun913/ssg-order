296f72105391423f7050e76d693cfe59337e5abd
syyun <sy.yun913@gmail.com>
2025-06-02 18:39:58 +0900

    feat: Redis store 추가

---
+++ src/main/java/com/ssg/order/domain/cache/CacheStore.java
@@ -0,0 +1,30 @@
+package com.ssg.order.domain.cache;
+
+import java.time.Duration;
+
+public interface CacheStore {
+    void put(
+        String key,
+        Object value
+    );
+
+    /**
+     * expiryTime 만큼 캐시를 저장한다.
+     */
+    void put(
+        String key,
+        Object value,
+        Duration expiryTime
+    );
+
+    String get(String key);
+
+    <T> T get(
+        String key,
+        Class<T> valueType
+    );
+
+    boolean exists(String key);
+
+    void delete(String key);
+}
